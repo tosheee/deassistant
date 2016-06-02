2d3a1948cf4fa07b77d27b43211e5534a4b3892c
Todor Chakarov <tosheee@abv.bg>
2016-06-02 11:28:28 +0300

    Change the logic in navigation products

--- app/controllers/normalizations_controller.rb
+++ app/controllers/normalizations_controller.rb
@@ -3,4 +3,9 @@ class NormalizationsController < ApplicationController
   def index
     @products = Admin::Product.all
   end
+
+  def show
+    @category = Admin::ProductCategory.find(params[:id])
+    @products = Admin::Product.where(product_category_id: @category ).pluck(:id, :product_name)
+  end
 end
