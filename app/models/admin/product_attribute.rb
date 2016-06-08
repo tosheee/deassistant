bc3d92fa48a10ae89a8797fdd97abcb34c15649b
Todor Chakarov <tosheee@abv.bg>
2016-06-08 09:06:03 +0300

    Add validation for ProductAttribute

--- app/models/admin/product_attribute.rb
+++ app/models/admin/product_attribute.rb
@@ -1,2 +1,4 @@
 class Admin::ProductAttribute < ActiveRecord::Base
+  validates :product_id, :name_attribute, presence: true
+  validates_uniqueness_of :product_id, :scope =>  [:name_attribute]
 end
