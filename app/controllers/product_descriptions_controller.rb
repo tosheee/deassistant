a0f29428142be77dee31d0ec65d20afd222d01f2
Todor Chakarov <tosheee@abv.bg>
2016-06-11 17:17:12 +0300

    Fix num view

--- app/controllers/product_descriptions_controller.rb
+++ app/controllers/product_descriptions_controller.rb
@@ -2,16 +2,6 @@ class ProductDescriptionsController < ApplicationController
 
   def list
        @products = Admin::Product.find(params[:id])
-
-       @product_attributes = Admin::ProductAttribute.where(product_id: params[:id])
-
-        #render text: @product_attributes.each {|attr|  attr.name_attribute.inspect }
-
-
-    #render text: "#{@products.id} #{@products.product_name}"
-    #@product = Admin::Product.where(id: params[:id]).pluck(:id, :product_name)
-     #@product_attributes = Admin::ProductAttribute.all
-     #@product_attributes = Admin::ProductAttribute.where(product_id: params[:id])
-     #@product_attributes = Admin::ProductAttribute.where(product_id: params[:id]).pluck(:id, :name_attribute, :attribute_description)
-  end
+       @product_attributes = Admin::ProductAttribute.where(product_id: params[:id]).order("num_view ASC")
+ end
 end
