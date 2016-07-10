3387162ee0a9ed55a36e652a875ab594de91eb0d
Todor Chakarov <tosheee@abv.bg>
2016-07-11 00:07:12 +0300

    Chage view feature brand

--- app/controllers/product_descriptions_controller.rb
+++ app/controllers/product_descriptions_controller.rb
@@ -7,7 +7,6 @@ class ProductDescriptionsController < ApplicationController
   end
 
   def show
-    #@products = Admin::Product.find(params[:id])
     @features_brands = Admin::FeaturesBrand.where(product_id: params[:id])
     @features_brand_attributes = Admin::FeatureBrandAttribute.where(features_brands_id: params[:id])
   end
