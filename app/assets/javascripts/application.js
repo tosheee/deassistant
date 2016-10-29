a2fabd8c80ce4b3f8b1a764a6617dc0863a9b870
Todor Chakarov <tosheee@abv.bg>
2016-10-29 12:46:31 +0300

    Add other style in form product attributes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,36 @@ function input(){
     var text = "here the text that you want to input.";
     document.forms.form1.area.value = text;
 }
+
+var textArea = []
+textArea.push(document.forms[0].getElementsByClassName('styled')[0].value);
+function input_li(){
+    if ( textArea.length > 0){
+        textArea.push("<li></li>");
+        document.forms[0].getElementsByClassName('styled')[0].value = textArea.toString();
+    }else{
+        textArea.push(document.forms[0].getElementsByClassName('styled')[0].value);
+        textArea.push("<li></li>");
+        document.forms[0].getElementsByClassName('styled')[0].value = textArea.toString();
+    }
+}
+function input_p(){
+    if ( textArea.length > 0){
+        textArea.push("<p></p>");
+        document.forms[0].getElementsByClassName('styled')[0].value = textArea.toString();
+    }else{
+        textArea.push(document.forms[0].getElementsByClassName('styled')[0].value);
+        textArea.push("<p></p>");
+        document.forms[0].getElementsByClassName('styled')[0].value = textArea.toString();
+    }
+}
+function input_a(){
+    if ( textArea.length > 0){
+        textArea.push("<a></a>");
+        document.forms[0].getElementsByClassName('styled')[0].value = textArea.toString();
+    }else{
+        textArea.push(document.forms[0].getElementsByClassName('styled')[0].value);
+        textArea.push("<a></a>");
+        document.forms[0].getElementsByClassName('styled')[0].value = textArea.toString();
+    }
+}
